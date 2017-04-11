e5cf9f5d11cbe024fd1210e0fb6218152c28db85
venkatahex <venkataramanae@hexaware.com>
2017-04-11 17:56:49 +0530

    Add files via upload
    snapprewrite maven integration

---
+++ SNAPP Rewrite/SpringRest/src/com/springservice/controller/SpringServiceController.java
@@ -0,0 +1,17 @@
+package com.springservice.controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/service/greeting")
+public class SpringServiceController {
+
+public String getGreeting(@PathVariable String name)
+{
+String result="Hello "+name; 
+return result;
+
+}
+}
